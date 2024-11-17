15cee3304ab72d94de2e651b5af7265f756eab9b
shimizukobo <43zutaka32@gmail.com>
2024-11-17 16:43:12 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -101,7 +101,7 @@ function renderPlaces(places, pos) {
         model.setAttribute('gltf-model', `${modelName}`);
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', `${cal.objectSize}`);
-//        model.setAttribute('position', '0 -${elevation} 0');
+        model.setAttribute('position', '0 -${elevation} 0');
 //        model.setAttribute('position', '0 0 0');
 
         model.addEventListener('loaded', () => {
