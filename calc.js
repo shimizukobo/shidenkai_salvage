f0f3fd705f71997accb931e7e6307198c8d20167
shimizukobo <43zutaka32@gmail.com>
2024-11-17 09:35:48 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -96,6 +96,8 @@ function renderPlaces(places, pos) {
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', `${cal.objectSize}`);
         model.setAttribute('position', '0 -${elevation} 0');
+        model.setAttribute('position', '0 -${elevation} 0');
+        model.setAttribute('rotation', '0 0 180');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
