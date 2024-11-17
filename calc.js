97d8cab4313ac4dec8186aeb201476b8d2e2d8bb
shimizukobo <43zutaka32@gmail.com>
2024-11-17 19:38:14 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -107,9 +107,9 @@ function renderPlaces(places, pos) {
             model.setAttribute('position', '0 -${elevation} 0');
         }
 
-//        model.setAttribute('position', '0 -${elevation} 0');
+        model.setAttribute('position', '0 -${elevation} 0');
 //        model.setAttribute('position', '0 0 0');
-        model.setAttribute('position', '0 -140 0');
+//        model.setAttribute('position', '0 -140 0');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
