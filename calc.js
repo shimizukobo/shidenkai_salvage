a7bc12ede44f3f32aa59505452cc7e0078958af1
shimizukobo <43zutaka32@gmail.com>
2024-11-20 20:13:10 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -79,8 +79,6 @@ function renderPlaces(places, pos) {
     var crd = pos.coords;
     let cal = new CalcVR();
 
-/*
-alert("1");
     //まず現在地の緯度経度を取得する
     var lat = pos.coords.latitude;
     var lon = pos.coords.longitude;
@@ -94,27 +92,27 @@ alert("1");
     const url = 'http://cyberjapandata2.gsi.go.jp/general/dem/scripts/getelevation.php?lon=' + stringLon + '&lat=' + stringLat + '&outtype=JSON';
 alert("2");
     fetch(url)
-    .then(function(response) {
+    .then(response=> {
         alert("OK");
       return response.json();
     })
-    .then(function(text) {   
+    .then(text=>{   
     //取得したjsonをパース
       var jsonAltitude = JSON.stringify(text);
         alert("緯度 " + pos.coords.latitude + "\n経度 " + pos.coords.longitude + "\n標高 " + jsonAltitude);
-　　　.catch(function(err1) { //失敗時に実行される
+    })
+    .catch(err1=>{ //失敗時に実行される
         alert("Error");
-    });
-*/
-	
+    });	
 
+/*
 var jsonAltitude = pos.coords.altitude;
 alert("緯度 " + pos.coords.latitude + "\n経度 " + pos.coords.longitude + "\n標高 " + jsonAltitude);
 if(jsonAltitude == 'undefind') {
 //if(jsonAltitude == null) {
     jsonAltitude = 0;
 }
-
+*/
     
     places.forEach((place) => {
         let latitude = place.location.lat;
