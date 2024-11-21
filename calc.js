1b886e0f67d1937521e1d09941a45ae41cc14bf4
shimizukobo <43zutaka32@gmail.com>
2024-11-22 08:19:41 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -119,7 +119,7 @@ alert("標高 " + jsonAltitude);
             jsonAltitude = jsonAltitude - 33;
 //            jsonAltitude = 1000;
         }
-alert("33\n緯度 " + pos.coords.latitude + "\n経度 " + pos.coords.longitude + "\n標高 " + jsonAltitude);
+alert("34\n緯度 " + pos.coords.latitude + "\n経度 " + pos.coords.longitude + "\n標高 " + jsonAltitude);
 jsonAltitude = jsonAltitude*2;
     });	
     
@@ -135,12 +135,12 @@ jsonAltitude = jsonAltitude*2;
         let model = document.createElement('a-entity');
 //        model.setAttribute('look-at', '[gps-camera]');    //正面を向ける
         model.setAttribute('look-at', '');    //向きを固定する
-        model.setAttribute('gps-entity-place', `latitude: ${cal.newPosition[0]}; longitude: ${cal.newPosition[1]};`);
-//        model.setAttribute('gps-entity-place', `latitude: ${place.location.lat}; longitude: ${place.location.lng};`);
-        model.setAttribute('gltf-model', `${modelName}`);
+        model.setAttribute('gps-entity-place', 'latitude: ${cal.newPosition[0]}; longitude: ${cal.newPosition[1]};');
+//        model.setAttribute('gps-entity-place', 'latitude: ${place.location.lat}; longitude: ${place.location.lng};');
+        model.setAttribute('gltf-model', '${modelName}');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', `${cal.objectSize}`);
-//        model.setAttribute('position', `0 0 -${jsonAltitude}`);
+        model.setAttribute('scale', '${cal.objectSize}');
+//        model.setAttribute('position', '0 0 -${jsonAltitude}');
         model.setAttribute('position', '0 -${jsonAltitude} 0');
 
         model.addEventListener('loaded', () => {
